da099ec4cfb522f34c1d5c5ac21454de785ed28a
EC2 Default User <ec2-user@ip-172-31-88-92.ec2.internal>
2022-10-10 12:25:43 +0000

    Sign up/Sign in 完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
   devise_for :users
   get 'home/top'
   get 'home/about'
   root to: 'home#top'
+  resources :users, only: [:index, :show, :edit] # 追加
 end
